325ba9deac76c6158c34e7d8d84c234d6d579498
Mr_Huan <huanzi20100702@hotmail.com>
2020-07-07 23:54:45 +0800

    整理入口文件

--- main.go
+++ main.go
@@ -13,33 +13,10 @@ import (
 )
 
 func init() {
-	dbUser := beego.AppConfig.String("db::dbUser")
-	dbPass := beego.AppConfig.String("db::dbPass")
-	dbHost := beego.AppConfig.String("db::dbHost")
-	dbPort := beego.AppConfig.String("db::dbPort")
-	dbName := beego.AppConfig.String("db::dbName")
-	dbStr := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8"
-	orm.Debug = true
-	ormErr := orm.RegisterDriver("mysql", orm.DRMySQL)
-	if ormErr != nil {
-		log.Println("ORM registration failed.")
-		return
-	}
-	ormErr = orm.RegisterDataBase("default", "mysql", dbStr)
-	if ormErr != nil {
-		log.Println("Register DataBase failed.")
-		return
-	}
-	// 设置为 UTC 时间
-	orm.DefaultTimeLoc = time.UTC
+	// 初始化ORM
+	initOrmDriver()
 	//设置日志
-	logsErr := logs.SetLogger(logs.AdapterMultiFile, `{"filename":"./logs/admin.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
-	if logsErr != nil {
-		//日志模块初始化失败
-		log.Println("Beego log module initialization failed.")
-		return
-	}
-	logs.Async() //日志异步
+	initLogsDriver()
 	//初始化session
 	beego.BConfig.WebConfig.Session.SessionOn = true
 	lib.SessionInit()
@@ -67,4 +44,38 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.Run()
+}
+
+// 初始化ORM
+func initOrmDriver() {
+	dbUser := beego.AppConfig.String("db::dbUser")
+	dbPass := beego.AppConfig.String("db::dbPass")
+	dbHost := beego.AppConfig.String("db::dbHost")
+	dbPort := beego.AppConfig.String("db::dbPort")
+	dbName := beego.AppConfig.String("db::dbName")
+	dbStr := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8"
+	orm.Debug = true
+	ormErr := orm.RegisterDriver("mysql", orm.DRMySQL)
+	if ormErr != nil {
+		log.Println("ORM registration failed.")
+		return
+	}
+	ormErr = orm.RegisterDataBase("default", "mysql", dbStr)
+	if ormErr != nil {
+		log.Println("Register DataBase failed.")
+		return
+	}
+	// 设置为 UTC 时间
+	orm.DefaultTimeLoc = time.UTC
+}
+
+// 初始化日志
+func initLogsDriver() {
+	logsErr := logs.SetLogger(logs.AdapterMultiFile, `{"filename":"./logs/admin.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
+	if logsErr != nil {
+		//日志模块初始化失败
+		log.Println("Beego log module initialization failed.")
+		return
+	}
+	logs.Async() //日志异步
 }
