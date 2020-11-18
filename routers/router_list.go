82bac860a79307b15fbb3cf597a70ec8162c3abf
Mr_Huan <huanzi20100702@hotmail.com>
2020-11-18 20:46:58 +0800

    规则列表增加注释

--- routers/router_list.go
+++ routers/router_list.go
@@ -30,109 +30,130 @@ var RouterListInterface = []RouterList{
 		Method:     "post:Register",
 	},
 	//************************* 文章 *************************//
+	// 文章列表
 	{
 		Router:     "/article/list",
 		Controller: &cms.ArticleController{},
 		Method:     "get,post:List",
 	},
+	// 文章发布
 	{
 		Router:     "/article/release",
 		Controller: &cms.ArticleController{},
 		Method:     "post:Release",
 	},
+	// 文章修改
 	{
 		Router:     "/article/modify",
 		Controller: &cms.ArticleController{},
 		Method:     "post:Modify",
 	},
+	// 文章删除
 	{
 		Router:     "/article/delete",
 		Controller: &cms.ArticleController{},
 		Method:     "post:Delete",
 	},
 	//************************* 栏目 *************************//
+	// 栏目列表
 	{
 		Router:     "/category/list",
 		Controller: &cms.CategoryController{},
 		Method:     "get,post:List",
 	},
+	// 新增栏目
 	{
 		Router:     "/category/add",
 		Controller: &cms.CategoryController{},
 		Method:     "post:Add",
 	},
+	// 栏目修改
 	{
 		Router:     "/category/modify",
 		Controller: &cms.CategoryController{},
 		Method:     "post:Modify",
 	},
+	// 栏目删除
 	{
 		Router:     "/category/delete",
 		Controller: &cms.CategoryController{},
 		Method:     "post:Delete",
 	},
 	//************************* 用户 *************************//
+	// 用户列表
 	{
 		Router:     "/user/list",
 		Controller: &admin.UserController{},
 		Method:     "get,post:List",
 	},
+	// 创建用户
 	{
 		Router:     "/user/create",
 		Controller: &admin.UserController{},
 		Method:     "post:CreateUser",
 	},
+	// 用户信息修改
 	{
 		Router:     "/user/modify",
 		Controller: &admin.UserController{},
 		Method:     "post:Modify",
 	},
+	// 禁用用户
 	{
 		Router:     "/user/forbid",
 		Controller: &admin.UserController{},
 		Method:     "post:ForbidUser",
 	},
+	// 启用用户
 	{
 		Router:     "/user/resume",
 		Controller: &admin.UserController{},
 		Method:     "post:ResumeUser",
 	},
 	//************************* 权限规则 *************************//
+	// 权限规则列表
 	{
 		Router:     "/rule/list",
 		Controller: &admin.RuleController{},
 		Method:     "get,post:List",
 	},
+	// 新增规则
 	{
 		Router:     "/rule/add",
 		Controller: &admin.RuleController{},
 		Method:     "post:Add",
 	},
+	// 修改规则
 	{
 		Router:     "/rule/modify",
 		Controller: &admin.RuleController{},
 		Method:     "post:Modify",
 	},
+	// 写入权限组
 	{
 		Router:     "/rule/WriteGroup",
 		Controller: &admin.RuleController{},
 		Method:     "post:WriteGroup",
 	},
+	// 删除权限组
 	{
 		Router:     "/rule/DeleteGroup",
 		Controller: &admin.RuleController{},
 		Method:     "post:DeleteGroup",
 	},
+	// 权限组授权
 	{
 		Router:     "/rule/AccessAuth",
 		Controller: &admin.RuleController{},
 		Method:     "post:AccessAuth",
 	},
+	// 用户授权
 	{
 		Router:     "/rule/MemberAuth",
 		Controller: &admin.RuleController{},
 		Method:     "post:MemberAuth",
 	},
+	// 移除人员权限
 	{
 		Router:     "/rule/RemoveMemberAuth",
 		Controller: &admin.RuleController{},
